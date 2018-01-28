fd9781046c816a28d5aaa0f7f8c475b12805a394
TheLostKing <calderonezj@gmail.com>
2018-01-28 11:13:47 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        byte bite = 1;
+        short shoty = 23;
+        int inty = 765;
+        long looooong = 33434050;
+        float floating = 45.4f;
+        double doubletrouble = 43.5;
+        boolean bool = true;
+        char carrot = 'd';
+
+        System.out.println("Byte: " + bite + "\nShort: " + shoty + "\nInt: " + inty + "\nLong: " + looooong + "\nFloat: " + floating + "\nDouble: " + doubletrouble + "\nBoolean: " + bool + "Character: " + carrot);
+
+    }
+}
